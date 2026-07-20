9268cd9393d5b0241a18ce41d2db55fede7f8840
anditaurban <anditaurban@gmail.com>
2026-07-21 00:21:41 +0700

    feat: apply api prod integrating

--- assets/js/config.js
+++ assets/js/config.js
@@ -9,7 +9,7 @@
     window.PMD_API_BASE_URL ||
     (window.location.protocol === "file:" || localPreviewHost
       ? "http://localhost/service-papuans-manado/backend-API/api"
-      : "../backend-API/api");
+      : "http://api.servicepapuansmanado.store/api");
 
   const statusMeta = [
     {
